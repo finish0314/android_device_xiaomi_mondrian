7c878f1d0c519d42d509178c4e78affbb2cd1817
finish0314 <finish21190@gmail.com>
2024-11-22 17:58:24 +0800

    mondrian: Add Dolby blobs

--- extract-files.py
+++ extract-files.py
@@ -50,6 +50,9 @@ blob_fixups: blob_fixups_user_type = {
         'vendor.qti.hardware.display.config-V2-ndk_platform.so',
         'vendor.qti.hardware.display.config-V2-ndk.so',
     ),
+    'vendor/bin/hw/vendor.dolby.hardware.dms@2.0-service': blob_fixup().add_needed(
+    'libstagefright_foundation-v33.so',
+    )
 }
 
 module = ExtractUtilsModule(
